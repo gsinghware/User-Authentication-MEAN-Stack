e0c7e1fd7338160cdf1c7ed12a9518d9d64236a6
gurusingh <gurusingh@bitbucket.org>
2016-07-21 19:25:46 -0400

    user model pre-save password hash

--- models/user.js
+++ models/user.js
@@ -16,4 +16,38 @@ var userSchema = new Schema({
     }
 }, { timestamps: true });
 
+ /**   
+  * The purpose of this middleware is to hash the password before 
+  * saving to database
+  */
+
+var bcrypt = require('bcrypt-nodejs');
+
+userSchema.pre('save', function (next) {
+	var user = this;
+	var SALT_FACTOR = 10;
+
+	if (!user.isModified('local.password')) 
+    	return next();
+    
+    bcrypt.genSalt(SALT_FACTOR, function (error, salt) {
+    	if (error) return next(error);
+
+    	/* hash the password */
+	    bcrypt.hash(user.local.password, salt, null, function(error, hash) {
+	    	if (error) return next(error);
+
+            user.local.password = hash;
+    		next();
+	    });
+    });
+
+});
+
+/* compare password function */
+userSchema.methods.comparePassword = function (password) {
+	var user = this;
+	return bcrypt.compareSync(password, user.local.password);
+};
+
 module.exports = mongoose.model('User', userSchema);
